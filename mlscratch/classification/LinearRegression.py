0ad7b37e45ad88c972060bc040b3e69932d1386b
dimitar <depitropov@gmail.com>
2020-06-26 18:28:15 +0300

    Changed error calculation

--- mlscratch/classification/LinearRegression.py
+++ mlscratch/classification/LinearRegression.py
@@ -8,21 +8,27 @@ class LinearRegression:
         self.parameters = None
 
     def train(self, features, target, alpha, iterations):
-        self.parameters = self.gradient_descent(features, target, alpha, iterations)
-        
+        self.parameters = self._gradient_descent(features, target, alpha, iterations)
+
     def predict(self, features):
-        return features.dot(features)
+        result = []
+        for i in range(features.shape[0]):
+            result.append(features[i].dot(self.parameters))
+        return result
 
-    def calculate_error_bias(self, parameters, features, targets):
-        return features.shape[0] / 2 * (sum(features.dot(parameters) - targets))
+    def _calculate_error(self, parameters, features, targets):
+        result = 0
+        for i in range(features.shape[0]):
+            result = result + (features[i].dot(parameters) - targets[i]) ** 2
+        return result / features.shape[0]
 
-    def calculate_error(self, parameters, features, targets, parameter):
+    def _calculate_slope(self, parameters, features, targets, parameter):
         result = 0
         for i in range(features.shape[0]):
             result = result + (features[i].dot(parameters) - targets[i]) * features[i][parameter]
         return features.shape[0] / 2 * result
 
-    def gradient_descent(self, features, targets, alpha, iterations):
+    def _gradient_descent(self, features, targets, alpha, iterations):
         features = np.c_[np.ones((features.shape[0], 1)), features]
         current_parameters = np.ones((features.shape[1], 1))
         temp_parameters = np.ones((features.shape[1], 1))
@@ -30,14 +36,10 @@ class LinearRegression:
         for j in range(iterations):
             total_error = 0
             for parameter in range(current_parameters.shape[1] + 1):
-                if parameter == 0:
-                    error = self.calculate_error_bias(current_parameters, features, targets)
-                    total_error = total_error + error
-                    temp_parameters[parameter] = current_parameters[parameter] - alpha * error
-                else:
-                    error = self.calculate_error(current_parameters, features, targets, parameter)
-                    total_error = total_error + error
-                    temp_parameters[parameter] = current_parameters[parameter] - alpha * error
+                error = self._calculate_error(current_parameters, features, targets)
+                slope = self._calculate_slope(current_parameters, features, targets, parameter)
+                total_error = total_error + error
+                temp_parameters[parameter] = current_parameters[parameter] - alpha * slope
             current_parameters = temp_parameters
             print('>iteration=%d, error=%.3f' % (j, total_error))
         return current_parameters
